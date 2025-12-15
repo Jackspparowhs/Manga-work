4210f8967cc00274467d6774044d284314a40ac0
Jack Sparrow <108980635+Jackspparowhs@users.noreply.github.com>
2025-12-15 21:22:36 +0530

    Update index.tsx

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -3,11 +3,16 @@ import { Horizontal, Vertical } from '../Sidebar'
 const Navigation = () => {
   return (
       <>
-        <nav className="hidden max-w-min h-full md:block md:col-start-1 md:col-end-4 nav_wrapper">
+        {/* Desktop Sidebar (Left side) */}
+        <nav className="hidden h-full md:block md:col-start-1 md:col-end-4 nav_wrapper border-r border-gray-800">
           <Vertical/>
         </nav>
 
-        <nav className="block fixed bottom-0 left-0 w-full h-min z-50 md:hidden nav_wrapper">
+        {/* Mobile Bottom Bar (Fixed) */}
+        <nav 
+          className="block fixed bottom-0 left-0 w-full z-50 md:hidden border-t border-gray-800 bg-[#121212]/95 backdrop-blur-md"
+          style={{ paddingBottom: 'env(safe-area-inset-bottom)' }} /* This fixes the iPhone bottom bar issue */
+        >
           <Horizontal/>
         </nav>
       </>
